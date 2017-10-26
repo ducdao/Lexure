7de13a31b06948231e48d8a98fc4a2360a52b1f8
TommyCP <twhite07@calpoly.edu>
2017-10-26 01:11:56 -0700

    Added scanner for input/output file names, changed README

--- src/main/java/Transcribe.java
+++ src/main/java/Transcribe.java
@@ -1,7 +1,5 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Scanner;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
@@ -10,27 +8,49 @@ import edu.cmu.sphinx.result.WordResult;
 
 public class Transcribe {
 
+   private static String OUTPUT_DIR = "output/";
+
    public static void main(String[] args) throws Exception {
 
       Configuration configuration = new Configuration();
 
-      // Set path to acoustic model
+      final Scanner scanner = new Scanner(System.in);
+
+      // Set path to acoustic model, dictionary, and language model, then initialize recognizer.
       configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-
-      // Set path to dictionary
       configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-
-      // Set language model
       configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
       StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-      InputStream stream = new FileInputStream(new File("wideband.wav"));
 
-      recognizer.startRecognition(stream);
+      System.out.println("Welcome to the Lexure audio file transcriber!\n");
+
+      // Setup input
+      String fname = null;
+      while (true) {
+         try {
+            fname = prompt(scanner, "Please input the file name to be transcribed.");
+            System.out.println(fname);
+            InputStream stream = new FileInputStream(new File(fname));
+            recognizer.startRecognition(stream);
+            break;
+         } catch (FileNotFoundException e) {
+            System.out.printf("File %s not found. Please enter another filename to try or type quit to quit. \n", fname);
+         }
+      }
       SpeechResult result;
 
-      // Setup output file
-      PrintWriter writer = new PrintWriter("output", "UTF-8");
+      // Setup output
+      PrintWriter writer;
+      while (true) {
+         try {
+            fname = prompt(scanner, "Please input the file name to be used for output relative to the output folder.");
+            writer = new PrintWriter(OUTPUT_DIR + fname, "UTF-8");
+            break;
+         } catch (FileNotFoundException e) {
+            System.err.printf("Unable to open %s. Please enter another filename to try. \n", fname);
+         }
+      }
 
       while ((result = recognizer.getResult()) != null) {
          System.out.format("Hypothesis: %s\n", result.getHypothesis());
@@ -46,5 +66,15 @@ public class Transcribe {
 
       writer.close();
       recognizer.stopRecognition();
+   }
+
+   public static String prompt(Scanner console, String s) {
+      System.out.println(s);
+      String out = console.nextLine();
+      if (out.equalsIgnoreCase("quit")) {
+         System.out.println("Thank you for using the Lexure audio file transcriber.");
+         System.exit(0);
+      }
+      return out;
    }
 }
