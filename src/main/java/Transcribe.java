6ae402304c18c309253de508ad8b0f28d2e95eae
Duc Dao <ducdao11@gmail.com>
2017-10-31 11:55:03 -0700

    Creates output directory if it doesn't exist. Will now keep running until the user types 'quit'.

--- src/main/java/Transcribe.java
+++ src/main/java/Transcribe.java
@@ -8,8 +8,7 @@ import edu.cmu.sphinx.result.WordResult;
 
 public class Transcribe {
 
-   // TODO: Create OUTPUT_DIR when directory doesn't exist
-   private static String OUTPUT_DIR = "output/";
+   private static String OUTPUT_DIR = "output";
 
    public static void main(String[] args) throws Exception {
 
@@ -26,56 +25,79 @@ public class Transcribe {
 
       System.out.println("Welcome to the Lexure audio file transcriber!\n");
 
-      // Setup input
-      String fname = null;
+      createOutputDir();
+
+      // CLI
       while (true) {
-         try {
-            fname = prompt(scanner, "Please input the file name to be transcribed.");
-            System.out.println(fname);
-            InputStream stream = new FileInputStream(new File(fname));
-            recognizer.startRecognition(stream);
-            break;
-         } catch (FileNotFoundException e) {
-            System.out.printf("File %s not found. Please enter another filename to try or type quit to quit. \n", fname);
+         // Setup input
+         String fname = null;
+         while (true) {
+            try {
+               fname = prompt(scanner, "Please input the file name to be transcribed: ");
+               System.out.printf("Input File: %s", fname);
+               InputStream stream = new FileInputStream(new File(fname));
+               recognizer.startRecognition(stream);
+               break;
+            } catch (FileNotFoundException e) {
+               System.out.printf("File %s not found. Please enter another filename to try or type quit to quit. \n", fname);
+            }
          }
-      }
-      SpeechResult result;
+         SpeechResult result;
 
-      // Setup output
-      PrintWriter writer;
-      while (true) {
-         try {
-            fname = prompt(scanner, "Please input the file name to be used for output relative to the output folder.");
-            writer = new PrintWriter(OUTPUT_DIR + fname, "UTF-8");
-            break;
-         } catch (FileNotFoundException e) {
-            System.err.printf("Unable to open %s. Please enter another filename to try. \n", fname);
-         }
-      }
-
-      while ((result = recognizer.getResult()) != null) {
-         System.out.format("Hypothesis: %s\n", result.getHypothesis());
-
-         // Get individual words and their times.
-         for (WordResult r : result.getWords()) {
-            System.out.println(r);
+         // Setup output
+         PrintWriter writer;
+         while (true) {
+            try {
+               fname = prompt(scanner, "Please input the file name to be used for output relative to the output folder: ");
+               System.out.printf("Output File: %s\n\n", fname);
+               writer = new PrintWriter(OUTPUT_DIR + "/" + fname, "UTF-8");
+               break;
+            } catch (FileNotFoundException e) {
+               System.err.printf("Unable to open %s. Please enter another filename to try. \n", fname);
+            }
          }
 
-         // Write to output file
-         writer.println(result.getHypothesis());
-      }
+         System.out.println("Lexure now transcribing...\n\n");
 
-      writer.close();
-      recognizer.stopRecognition();
+         while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+
+            // Get individual words and their times.
+            for (WordResult r : result.getWords()) {
+               System.out.println(r);
+            }
+
+            // Write to output file
+            writer.println(result.getHypothesis());
+         }
+
+         writer.close();
+         recognizer.stopRecognition();
+
+         System.out.println("Transcription complete!\n");
+      }
    }
 
    public static String prompt(Scanner console, String s) {
-      System.out.println(s);
+      System.out.print(s);
       String out = console.nextLine();
       if (out.equalsIgnoreCase("quit")) {
-         System.out.println("Thank you for using the Lexure audio file transcriber.");
+         System.out.println("\nThank you for using the Lexure audio file transcriber.");
          System.exit(0);
       }
       return out;
+   }
+
+   public static void createOutputDir() {
+      File directory = new File(OUTPUT_DIR);
+
+      // Create new directory for transcriptions
+      if (!directory.exists()) {
+         try {
+            directory.mkdir();
+         } catch (SecurityException se) {
+            System.err.printf("Unable to create output directory %s.\n", OUTPUT_DIR);
+         }
+      }
    }
 }
